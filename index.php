dd838d435cc18418245322d583f2a076fc9ce8aa
Nanditha Nambiar <nanditha0408@gmail.com>
2022-10-22 17:48:08 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,305 +1,103 @@
-<?php 
+﻿<?php 
 
-require "admin/includes/dbh.php";
+include "includes/unset-sessions.php";
 
 ?>
 
 <!DOCTYPE html>
-<html class="no-js" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-
-    <!--- basic page needs
-    ================================================== -->
-    <meta charset="utf-8">
-    <title>NRN's Blog</title>
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- mobile specific metas
-    ================================================== -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSS
-    ================================================== -->
-    <link rel="stylesheet" href="css/vendor.css">
-    <link rel="stylesheet" href="css/styles.css">
-
-    <!-- script
-    ================================================== -->
-    <script src="js/modernizr.js"></script>
-    <script defer src="js/fontawesome/all.min.js"></script>
-
-    <!-- favicons
-    ================================================== -->
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
-    <link rel="manifest" href="site.webmanifest">
-
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Admin Side</title>
+    <!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- Morris Chart Styles-->
+    <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <!-- Custom Styles-->
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+    <!-- Google Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 </head>
 
-<body id="top">
+<body>
+    <div id="wrapper">
+
+    <?php include "header.php"; include "sidebar.php"; ?>
+
+        <div id="page-wrapper">
+            <div id="page-inner">
 
 
-    <!-- preloader
-    ================================================== -->
-    <div id="preloader"> 
-    	<div id="loader"></div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                            Dashboard
+                        </h1>
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <div class="panel panel-primary text-center no-boder bg-color-green">
+                            <div class="panel-body">
+                                <i class="fa fa-bar-chart-o fa-5x"></i>
+                                <h3>8,457</h3>
+                            </div>
+                            <div class="panel-footer back-footer-green">
+                                Total Visits
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <div class="panel panel-primary text-center no-boder bg-color-blue">
+                            <div class="panel-body">
+                                <i class="fa fa-book fa-5x"></i>
+                                <h3>52,160 </h3>
+                            </div>
+                            <div class="panel-footer back-footer-blue">
+                                Blogs
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <div class="panel panel-primary text-center no-boder bg-color-red">
+                            <div class="panel-body">
+                                <i class="fa fa fa-comments fa-5x"></i>
+                                <h3>15,823 </h3>
+                            </div>
+                            <div class="panel-footer back-footer-red">
+                                Comments
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <?php include "footer.php"; ?>
+				
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
     </div>
+    <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+    <!-- Morris Chart Js -->
+    <script src="assets/js/morris/raphael-2.1.0.min.js"></script>
+    <script src="assets/js/morris/morris.js"></script>
+    <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
 
-    <?php include "header.php"; ?>
-
-    <!-- hero
-    ================================================== -->
-    <section id="hero" class="s-hero">
-
-        <div class="s-hero__slider">
-
-            <?php 
-            
-            $sqlGetFirstBlog = "SELECT * FROM blog_post INNER JOIN blog_category ON blog_post.n_category_id = blog_category.n_category_id WHERE n_home_page_placement = '1' AND f_post_status != '2' LIMIT 1";
-            $queryGetFirstBlog = mysqli_query($conn, $sqlGetFirstBlog);
-
-            if ($rowGetFirstBlog = mysqli_fetch_assoc($queryGetFirstBlog)) {
-
-                $firstBlogCategory = $rowGetFirstBlog['v_category_title'];
-                $firstBlogCategoryPath = $rowGetFirstBlog['v_category_path'];
-                $firstBlogTitle = $rowGetFirstBlog['v_post_title'];
-                $firstBlogPath = $rowGetFirstBlog['v_post_path'];
-                $firstBlogMainImageUrl = $rowGetFirstBlog['v_main_image_url'];
-            ?>
-
-            <div class="s-hero__slide">
-                <div class="s-hero__slide-bg" style="background-image: url('<?php echo $firstBlogMainImageUrl; ?>');"></div>
-                <div class="row s-hero__slide-content animate-this">
-                    <div class="column">
-                        <div class="s-hero__slide-meta">
-                            <span class="cat-links">
-                                <a href="categories.php?group=<?php echo $firstBlogCategoryPath; ?>"><?php echo $firstBlogCategory; ?></a>
-                            </span>
-                            <span class="byline"> 
-                                Posted by 
-                                <span class="author">
-                                    <a href="#">NRN</a>
-                                </span>
-                            </span>
-                        </div>
-                        <h1 class="s-hero__slide-text">
-                            <a href="single-blog.php?blog=<?php echo $firstBlogPath; ?>"><?php echo $firstBlogTitle; ?></a>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-
-            <?php
-            }
-            
-            $sqlGetSecondBlog = "SELECT * FROM blog_post INNER JOIN blog_category ON blog_post.n_category_id = blog_category.n_category_id WHERE n_home_page_placement = '2' AND f_post_status != '2' LIMIT 1";
-            $queryGetSecondBlog = mysqli_query($conn, $sqlGetSecondBlog);
-
-            if ($rowGetSecondBlog = mysqli_fetch_assoc($queryGetSecondBlog)) {
-
-                $secondBlogCategory = $rowGetSecondBlog['v_category_title'];
-                $secondBlogCategoryPath = $rowGetSecondBlog['v_category_path'];
-                $secondBlogTitle = $rowGetSecondBlog['v_post_title'];
-                $secondBlogPath = $rowGetSecondBlog['v_post_path'];
-                $secondBlogMainImageUrl = $rowGetSecondBlog['v_main_image_url'];
-            
-            ?>
-
-            <div class="s-hero__slide">
-                <div class="s-hero__slide-bg" style="background-image: url('<?php echo $secondBlogMainImageUrl; ?>');"></div>
-                <div class="row s-hero__slide-content animate-this">
-                    <div class="column">
-                        <div class="s-hero__slide-meta">
-                            <span class="cat-links">
-                                <a href="categories.php?group=<?php echo $secondBlogCategoryPath; ?>"><?php echo $secondBlogCategory; ?></a>
-                            </span>
-                            <span class="byline"> 
-                                Posted by 
-                                <span class="author">
-                                    <a href="#">NRN</a>
-                                </span>
-                            </span>
-                        </div>
-                        <h1 class="s-hero__slide-text">
-                            <a href="single-blog.php?blog=<?php echo $secondBlogPath; ?>"><?php echo $secondBlogTitle; ?></a>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-
-            <?php 
-            }
-
-            $sqlGetThirdBlog = "SELECT * FROM blog_post INNER JOIN blog_category ON blog_post.n_category_id = blog_category.n_category_id WHERE n_home_page_placement = '3' AND f_post_status != '2' LIMIT 1";
-            $queryGetThirdBlog = mysqli_query($conn, $sqlGetThirdBlog);
-
-            if ($rowGetThirdBlog = mysqli_fetch_assoc($queryGetThirdBlog)) {
-
-                $thirdBlogCategory = $rowGetThirdBlog['v_category_title'];
-                $thirdBlogCategoryPath = $rowGetThirdBlog['v_category_path'];
-                $thirdBlogTitle = $rowGetThirdBlog['v_post_title'];
-                $thirdBlogPath = $rowGetThirdBlog['v_post_path'];
-                $thirdBlogMainImageUrl = $rowGetThirdBlog['v_main_image_url'];
-            
-            ?>
-
-            <div class="s-hero__slide"">
-                <div class="s-hero__slide-bg" style="background-image: url('<?php echo $thirdBlogMainImageUrl; ?>');"></div>
-                <div class="row s-hero__slide-content animate-this">
-                    <div class="column">
-                        <div class="s-hero__slide-meta">
-                            <span class="cat-links">
-                                <a href="categories.php?group=<?php echo $thirdBlogCategoryPath; ?>"><?php echo $thirdBlogCategory; ?></a>
-                            </span>
-                            <span class="byline"> 
-                                Posted by 
-                                <span class="author">
-                                    <a href="#">NRN</a>
-                                </span>
-                            </span>
-                        </div>
-                        <h1 class="s-hero__slide-text">
-                            <a href="single-blog.php?blog=<?php echo $thirdBlogPath; ?>"><?php echo $thirdBlogTitle; ?></a>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-
-            <?php } ?>
-
-        </div> <!-- end s-hero__slider -->
-
-        <div class="s-hero__social hide-on-mobile-small">
-            <p>Follow</p>
-            <span></span>
-            <ul class="s-hero__social-icons">
-                <li><a href="https://facebook.com"><i class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
-                <li><a href="https://twitter.com"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
-                <li><a href="https://instagram.com"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
-                <li><a href="https://youtube.com"><i class="fab fa-youtube" aria-hidden="true"></i></a></li>
-            </ul>
-        </div> <!-- end s-hero__social -->
-
-        <div class="nav-arrows s-hero__nav-arrows">
-            <button class="s-hero__arrow-prev">
-                <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M1.5 7.5l4-4m-4 4l4 4m-4-4H14" stroke="currentColor"></path></svg>
-            </button>
-            <button class="s-hero__arrow-next">
-               <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg" width="15" height="15"><path d="M13.5 7.5l-4-4m4 4l-4 4m4-4H1" stroke="currentColor"></path></svg>
-            </button>
-        </div> <!-- end s-hero__arrows -->
-
-    </section> <!-- end s-hero -->
-
-
-    <!-- content
-    ================================================== -->
-    <section class="s-content s-content--no-top-padding">
-
-
-        <!-- masonry
-        ================================================== -->
-        <div class="s-bricks">
-
-            <div class="masonry">
-                <div class="bricks-wrapper h-group">
-
-                    <div class="grid-sizer"></div>
-
-                    <div class="lines">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-
-                    <?php 
-                    
-                    $sqlGetAllBlogs = "SELECT * FROM blog_post WHERE f_post_status = '1' ORDER BY n_blog_post_id";
-                    $queryGetAllBlogs = mysqli_query($conn, $sqlGetAllBlogs);
-
-                    while ($rowGetAllBlogs = mysqli_fetch_assoc($queryGetAllBlogs)) {
-
-                        $blogTitle = $rowGetAllBlogs['v_post_title'];
-                        $blogPath = $rowGetAllBlogs['v_post_path'];
-                        $blogSummary = $rowGetAllBlogs['v_post_summary'];
-                        $blogAltImageUrl = $rowGetAllBlogs['v_alt_image_url'];
-                    
-                    ?>
-
-                    <article class="brick entry" data-aos="fade-up">
-    
-                        <div class="entry__thumb">
-                            <a href="single-blog.php?blog=<?php echo $blogPath; ?>" class="thumb-link">
-                                <img src="<?php echo $blogAltImageUrl; ?>" 
-                                     srcset="<?php echo $blogAltImageUrl; ?> 1x, <?php echo $blogAltImageUrl; ?> 2x" alt="">
-                            </a>
-                        </div> <!-- end entry__thumb -->
-    
-                        <div class="entry__text">
-                            <div class="entry__header">
-                                <h1 class="entry__title"><a href="single-blog.php?blog=<?php echo $blogPath; ?>"><?php echo $blogTitle; ?></a></h1>
-                                
-                                <div class="entry__meta">
-                                    <span class="byline"">By:
-                                        <span class='author'>
-                                            <a href="#">NRN</a>
-                                        </span>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="entry__excerpt">
-                                <p><?php echo $blogSummary; ?></p>
-                            </div>
-                            <a class="entry__more-link" href="single-blog.php?blog=<?php echo $blogPath; ?>">Read Blog</a>
-                        </div> <!-- end entry__text -->
-                    
-                    </article> <!-- end article -->
-
-                    <?php } ?>
-
-                </div> <!-- end brick-wrapper -->
-
-            </div> <!-- end masonry -->
-
-            <div class="row">
-                <div class="column large-12">
-                    <nav class="pgn">
-                        <ul>
-                            <li>
-                                <span class="pgn__prev" href="#0">
-                                    Prev
-                                </span>
-                            </li>
-                            <li><a class="pgn__num" href="#0">1</a></li>
-                            <li><span class="pgn__num current">2</span></li>
-                            <li><a class="pgn__num" href="#0">3</a></li>
-                            <li><a class="pgn__num" href="#0">4</a></li>
-                            <li><a class="pgn__num" href="#0">5</a></li>
-                            <li><span class="pgn__num dots">…</span></li>
-                            <li><a class="pgn__num" href="#0">8</a></li>
-                            <li>
-                                <span class="pgn__next" href="#0">
-                                    Next
-                                </span>
-                            </li>
-                        </ul>
-                    </nav> <!-- end pgn -->
-                </div> <!-- end column -->
-            </div> <!-- end row -->
-
-        </div> <!-- end s-bricks -->
-
-    </section> <!-- end s-content -->
-
-    <?php include "footer.php"; ?>
-
-    <!-- Java Script
-    ================================================== -->
-    <script src="js/jquery-3.5.0.min.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script>
 
 </body>
 
